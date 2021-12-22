b7180a09bc7815ff9245856e282c4d35f57453b4
sustaitangel <sustaita_7_7@hotmail.com>
2021-12-22 12:58:39 +0100

    File with results

---
+++ script.sh
@@ -0,0 +1,131 @@
+
+cd 
+cd distrib-simgrid
+cd aleatorio_fcfs
+echo ********************************************aleatorio_fcfs**************************************************************
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd aleatorio_ljf
+echo ********************************************aleatorio_ljf**************************************************************
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd aleatorio_sjf
+echo ********************************************aleatorio_sjf************************************************************** 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+
+
+cd 
+cd distrib-simgrid
+cd ciclico_fcfs
+echo **************************************************ciclico_fcfs******************************************************************* 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd ciclico_ljf
+echo **************************************************ciclico_ljf******************************************************************* 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd ciclico_sjf
+echo **************************************************ciclico_sjf*******************************************************************   
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+
+
+
+cd 
+cd distrib-simgrid
+cd sqf_fcfs
+echo *****************************************************sqf_fcfs************************************************************************ 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd sqf_ljf
+echo *****************************************************sqf_ljf************************************************************************
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd sqf_sjf
+echo *****************************************************sqf_sjf************************************************************************
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+
+
+cd 
+cd distrib-simgrid
+cd trc_fcfs
+echo *********************************************************trc_fcfs********************************************************************** 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd trc_ljf
+echo *********************************************************trc_ljf**********************************************************************  
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd trc_sjf
+echo *********************************************************trc_sjf**********************************************************************  
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+
+
+
+cd 
+cd distrib-simgrid
+cd trrrc_fcfs
+echo ******************************************************************trrrc_fcfs***************************************************************** 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd trrrc_ljf
+echo ******************************************************************trrrc_ljf*****************************************************************
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
+cd 
+cd distrib-simgrid
+cd trrrc_sjf
+echo ******************************************************************trrrc_sjf***************************************************************** 
+./ejemplo platform-cluster.xml 0.2
+./ejemplo platform-cluster.xml 0.5
+./ejemplo platform-cluster.xml 0.7
+./ejemplo platform-cluster.xml 0.9
