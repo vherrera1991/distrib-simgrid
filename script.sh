3d4a2467a932fb1b51c5c2da9a8cab08964f4bc8
sustaitangel <sustaita_7_7@hotmail.com>
2021-12-26 16:40:23 +0100

    changes in script names of algorithms

--- script.sh
+++ script.sh
@@ -3,52 +3,52 @@ cd
 cd distrib-simgrid
 cd aleatorio_fcfs
 echo ********************************************aleatorio_fcfs**************************************************************
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./aleatorio_fcfs platform-cluster.xml 0.2
+./aleatorio_fcfs platform-cluster.xml 0.5
+./aleatorio_fcfs platform-cluster.xml 0.7
+./aleatorio_fcfs platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd aleatorio_ljf
 echo ********************************************aleatorio_ljf**************************************************************
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./aleatorio_ljf platform-cluster.xml 0.2
+./aleatorio_ljf platform-cluster.xml 0.5
+./aleatorio_ljf platform-cluster.xml 0.7
+./aleatorio_ljf platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd aleatorio_sjf
-echo ********************************************aleatorio_sjf************************************************************** 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+echo ********************************************aleatorio_stf************************************************************** 
+./aleatorio_stf platform-cluster.xml 0.2
+./aleatorio_stf platform-cluster.xml 0.5
+./aleatorio_stf platform-cluster.xml 0.7
+./aleatorio_stf platform-cluster.xml 0.9
 
 
 cd 
 cd distrib-simgrid
 cd ciclico_fcfs
 echo **************************************************ciclico_fcfs******************************************************************* 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./ciclico_fcfs platform-cluster.xml 0.2
+./ciclico_fcfs platform-cluster.xml 0.5
+./ciclico_fcfs platform-cluster.xml 0.7
+./ciclico_fcfs platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd ciclico_ljf
 echo **************************************************ciclico_ljf******************************************************************* 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./ciclico_ljf platform-cluster.xml 0.2
+./ciclico_ljf platform-cluster.xml 0.5
+./ciclico_ljf platform-cluster.xml 0.7
+./ciclico_ljf platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd ciclico_sjf
-echo **************************************************ciclico_sjf*******************************************************************   
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+echo **************************************************ciclico_stf*******************************************************************   
+./ciclico_stf platform-cluster.xml 0.2
+./ciclico_stf platform-cluster.xml 0.5
+./ciclico_stf platform-cluster.xml 0.7
+./ciclico_stf platform-cluster.xml 0.9
 
 
 
@@ -56,52 +56,52 @@ cd
 cd distrib-simgrid
 cd sqf_fcfs
 echo *****************************************************sqf_fcfs************************************************************************ 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./sqf_fcfs platform-cluster.xml 0.2
+./sqf_fcfs platform-cluster.xml 0.5
+./sqf_fcfs platform-cluster.xml 0.7
+./sqf_fcfs platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd sqf_ljf
 echo *****************************************************sqf_ljf************************************************************************
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./sqf_ljf platform-cluster.xml 0.2
+./sqf_ljf platform-cluster.xml 0.5
+./sqf_ljf platform-cluster.xml 0.7
+./sqf_ljf platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd sqf_sjf
-echo *****************************************************sqf_sjf************************************************************************
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+echo *****************************************************sqf_stf************************************************************************
+./sqf_stf platform-cluster.xml 0.2
+./sqf_stf platform-cluster.xml 0.5
+./sqf_stf platform-cluster.xml 0.7
+./sqf_stf platform-cluster.xml 0.9
 
 
 cd 
 cd distrib-simgrid
 cd trc_fcfs
 echo *********************************************************trc_fcfs********************************************************************** 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./trc_fcfs platform-cluster.xml 0.2
+./trc_fcfs platform-cluster.xml 0.5
+./trc_fcfs platform-cluster.xml 0.7
+./trc_fcfs platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd trc_ljf
 echo *********************************************************trc_ljf**********************************************************************  
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./trc_ljf platform-cluster.xml 0.2
+./trc_ljf platform-cluster.xml 0.5
+./trc_ljf platform-cluster.xml 0.7
+./trc_ljf platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd trc_sjf
-echo *********************************************************trc_sjf**********************************************************************  
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+echo *********************************************************trc_stf**********************************************************************  
+./trc_stf platform-cluster.xml 0.2
+./trc_stf platform-cluster.xml 0.5
+./trc_stf platform-cluster.xml 0.7
+./trc_stf platform-cluster.xml 0.9
 
 
 
@@ -109,23 +109,23 @@ cd
 cd distrib-simgrid
 cd trrrc_fcfs
 echo ******************************************************************trrrc_fcfs***************************************************************** 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./trrrc_fcfs platform-cluster.xml 0.2
+./trrrc_fcfs platform-cluster.xml 0.5
+./trrrc_fcfs platform-cluster.xml 0.7
+./trrrc_fcfs platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd trrrc_ljf
 echo ******************************************************************trrrc_ljf*****************************************************************
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+./trrrc_ljf platform-cluster.xml 0.2
+./trrrc_ljf platform-cluster.xml 0.5
+./trrrc_ljf platform-cluster.xml 0.7
+./trrrc_ljf platform-cluster.xml 0.9
 cd 
 cd distrib-simgrid
 cd trrrc_sjf
-echo ******************************************************************trrrc_sjf***************************************************************** 
-./ejemplo platform-cluster.xml 0.2
-./ejemplo platform-cluster.xml 0.5
-./ejemplo platform-cluster.xml 0.7
-./ejemplo platform-cluster.xml 0.9
+echo ******************************************************************trrrc_stf***************************************************************** 
+./trrrc_stf platform-cluster.xml 0.2
+./trrrc_stf platform-cluster.xml 0.5
+./trrrc_stf platform-cluster.xml 0.7
+./trrrc_stf platform-cluster.xml 0.9
