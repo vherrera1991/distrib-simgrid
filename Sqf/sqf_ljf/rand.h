4e589493d38b1e503b1e39892e3c46f7810ed9ff
Luis Angel Sustaita Guerrero <luis.sustaita@cimat.mx>
2021-12-26 16:41:27 +0100

    Floder organization

---
+++ Sqf/sqf_ljf/rand.h
@@ -0,0 +1,16 @@
+double uniform(void);
+double uniform_pos(void);
+void seed( long int n);
+double uniform_ab(double a, double b);
+int uniform_int(int i, int n);
+double exponential(double landa);
+double erlang(double x, double s);
+double hyperx(double x, double s);
+double normal(double x, double s);
+double ran_weibull ( const double a, const double k);
+double ran_gamma (const double k, const double b);
+double ran_gamma_int (const unsigned int a);
+static double gamma_large (const double a);
+static double gamma_frac (const double a);
+
+
